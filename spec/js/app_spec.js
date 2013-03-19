1c9f891d80b87b6a31753ca27df04ab8089ff0d1
Grzegorz Ziolkowski <gz@ziolo.eu>
2013-03-19 23:16:14 +0100

    Update just to recheck travis build

--- spec/js/app_spec.js
+++ spec/js/app_spec.js
@@ -1,12 +1,12 @@
 /*global describe, it, sinon */
 
-define([ 'js/app' ], function(main) {
+define([ 'js/app' ], function(app) {
   'use strict';
 
   describe('Main', function() {
     describe('Main init', function() {
       it('should be main equal Test', function() {
-        main.should.be.a('object');
+        app.should.be.a('object');
       });
     });
   });
