b7ee15188723225a0eec92af7a778354a739c293
Grzegorz Ziolkowski <gz@ziolo.eu>
2013-03-20 23:21:15 +0100

    Travis fixed. Readme updated. Handling for urlArgs added.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 require([ 'config' ], function() {
+  var getUrlArgs = function() {
+    var search = document.location.search || '';
+    if (search.indexOf('?') === 0) {
+      search = search.substring(1);
+    }
+    return search;
+  };
   require.config({
     deps : [ 'js/app' ],
     paths : {
       'js/app' : 'app'
-    }
+    },
+    urlArgs : getUrlArgs()
   });
 });
