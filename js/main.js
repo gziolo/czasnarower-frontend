d7e0e5717394452bede07360f5ef4061cd1f146d
gz <gz@ziolo.eu>
2013-03-22 11:37:30 +0100

    Fixed wrong merge of js/main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ require([ 'config' ], function() {
   var baseUrl = (window.staticUrl || '') + 'frontend' + (window.jsVersion || '') + '/';
   require.config({
     baseUrl : baseUrl,
-    deps : [ 'js/app' ],
-    paths : {
-      'js/app' : 'app'
-    }
+    deps : [ 'js/app' ]
   });
 });
