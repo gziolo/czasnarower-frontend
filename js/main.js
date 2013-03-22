827d2254f81a70767ea1be2fff213ca7073af4ac
gz <gz@ziolo.eu>
2013-03-22 09:52:07 +0100

    JSHint conventions added JS version code removed, versioning moved to url path handled by rewrite rule

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
 require([ 'config' ], function() {
-  var getUrlArgs = function() {
-    if (window.jsVersion === undefined) {
-      return '';
-    }
-    return 'v=' + window.jsVersion;
-  };
   require.config({
     deps : [ 'js/app' ],
     paths : {
       'js/app' : 'app'
-    },
-    urlArgs : getUrlArgs()
+    }
   });
 });
---
+++ tools/eclipse/README.md
@@ -0,0 +1,5 @@
+## Eclipse tools
+
+### Code refactor
+
+[JSHint Conventions.xml](https://github.com/gziolo/czasnarower-frontend/tree/master/tools/JSHint Conventions.xml) file contains settings for code style formatter.
