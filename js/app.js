24e31df2d806420c6696f10210b09f06bdbecc41
gz <gz@ziolo.eu>
2013-03-26 10:58:43 +0100

    Added FuelUX datagrid plugin package.json updated

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-define([ 'jquery_migrate', 'text', 'bootstrap', 'bootstrap_datepicker', 'bootstrap_lightbox', 'bootstrap_tag', 'backbone', 'moment_pl' ], function() {
+define([ 'jquery_migrate', 'text', 'bootstrap', 'bootstrap_datepicker', 'bootstrap_lightbox', 'bootstrap_tag', 'fuelux_datagrid', 'backbone', 'moment_pl' ], function() {
   require([ 'legacy/common'], function() {});
   return {};
 });
