e60b0e5c62773543b75083dc6e3a4ff800856994
gz <gz@ziolo.eu>
2013-04-08 09:20:54 +0200

    New tests for user race stats added.

--- spec/js/components/user_race_stats/component_spec.js
+++ spec/js/components/user_race_stats/component_spec.js
@@ -3,22 +3,83 @@ define([ 'jquery', 'user_race_stats/component' ], function($, UserRaceStats) {
 
   describe('User race stats', function() {
 
-    before(function() {
-      $('body').append('<div id="test"></div>');
-      this.instance = new UserRaceStats('#test');
+    describe('Data not provided', function() {
+
+      before(function() {
+        $('body').append('<div id="test"></div>');
+        this.instance = new UserRaceStats('#test');
+      });
+
+      it('has datagrid table', function() {
+        this.instance.$node.find('table.datagrid').length.should.equal(1);
+      });
+
+      it('has datagrid with proper id', function() {
+        this.instance.$node.find('table.datagrid').attr('id').should.equal('cnr-races-stats');
+      });
+
+      it('has datagrid table head with proper label', function() {
+        this.instance.$node.find('table.datagrid thead .datagrid-header-title').text().should.equal('Statystyki uczestników wyścigów');
+      });
+
+      it('has datagrid table body with proper message', function() {
+        this.instance.$node.find('table.datagrid tbody').text().should.equal('0 wierszy');
+      });
+
+      after(function() {
+        this.instance.teardown();
+        $('#test').remove();
+      });
     });
 
-    it('has datagrid table', function() {
-      this.instance.$node.find('table.datagrid').length.should.equal(1);
-    });
-    
-    it('has empty datagrid table tbody', function() {
-      this.instance.$node.find('table.datagrid tbody').text().should.equal('0 wierszy');
-    });
+    describe('Data provided', function() {
 
-    after(function() {
-      this.instance.teardown();
-      $('#test').remove();
+      before(function() {
+        $('body').append('<div id="test"></div>');
+        $('#test').data('stats', [ {
+          total_time : '04:02:33',
+          total_odo : '123',
+          races : '2',
+          avg_speed : '21',
+          nick : 'test user',
+          avatar_file_name : 'http://www.czasnarower.pl/photo/avatar/avatar_default.jpg',
+          url_view : 'http://www.czasnarower/uzytkownik/123'
+        }, {
+          total_time : '05:02:33',
+          total_odo : '120,5',
+          races : '3',
+          avg_speed : '18,5',
+          nick : 'test user2',
+          avatar_file_name : 'http://www.czasnarower.pl/photo/avatar/avatar_default.jpg',
+          url_view : 'http://www.czasnarower/uzytkownik/323'
+        } ]);
+        this.instance = new UserRaceStats('#test');
+      });
+
+      it('has datagrid with two rows', function() {
+        this.instance.$node.find('table.datagrid tbody tr').length.should.equal(2);
+      });
+
+      it('has datagrid with five columns', function() {
+        this.instance.$node.find('table.datagrid tbody tr:first td').length.should.equal(5);
+      });
+
+      it('has datagrid sorted by total time column', function() {
+        this.instance.$node.find('table.datagrid tbody tr:first > td:nth-child(2)').text().should.equal('05:02:33');
+      });
+
+      it('has datagrid with image in first column', function() {
+        this.instance.$node.find('table.datagrid tbody tr:first > td:nth-child(1) img').attr('src').should.equal('http://www.czasnarower.pl/photo/avatar/avatar_default.jpg');
+      });
+
+      it('has datagrid with link to user profile in first column', function() {
+        this.instance.$node.find('table.datagrid tbody tr:first > td:nth-child(1) a').attr('href').should.equal('http://www.czasnarower/uzytkownik/323');
+      });
+
+      after(function() {
+        this.instance.teardown();
+        $('#test').remove();
+      });
     });
 
   });
